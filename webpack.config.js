527dc60641345a49327faffef318a2bc0950bb67
Stacy Wang <swangny@gmail.com>
2024-04-16 12:22:42 -0700

    feat: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'build'), // change this
+    path: path.resolve(__dirname, 'dist'), // change this
     publicPath: '/',
     filename: 'bundle.js',
   },
